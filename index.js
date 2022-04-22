d8d6ad10e68136a95137a32d2d0de6870316b38d
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2022-04-21 23:06:53 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your code here!
-const main = main.remove()
+document.body.removeChild(main)
 const newHeader = document.createElement('h1')
 newHeader.id = 'victory'
 newHeader.innerHTML = 'is the champion'
+document.body.append(newHeader)
